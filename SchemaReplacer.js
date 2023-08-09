7ffbfd98660ed8f0b109db071411c9d7abd5548c
DarkerInk <rstonehouse66@gmail.com>
2023-08-09 16:02:55 -0500

    Some Improvements

--- SchemaReplacer.js
+++ SchemaReplacer.js
@@ -1,54 +1,70 @@
-const schemas = require('./OldSchemas/');
-const fs = require('fs');
+console.time('Time to generate CQL files');
+
+const schemas = require('./src/OldSchemas/Schemas');
+const fs = require('fs/promises');
 const path = require('path');
-const PathToCqlFiles = path.join(__dirname, 'cql');
+const PathToCqlFiles = path.join(__dirname, 'src', 'cql');
 
 const clearInArgs = process.argv.includes('--clear');
 const dryRun = process.argv.includes('--dry');
-const removeExtraNewLines = process.argv.includes('--removeExtraNewLines');
-
-const Clean = () => {
-    return new Promise((resolve, reject) => {
-        const files = fs.readdirSync(PathToCqlFiles);
-
-        for (const file of files) {
-            fs.unlinkSync(path.join(PathToCqlFiles, file));
-        }
-
-        resolve();
-    });
-};
+const clearExtraNewLines = process.argv.includes('--clear-extra-new-lines');
 
 const Main = async () => {
+    console.log('='.repeat(80));
+
     if (clearInArgs) {
-        await Clean();
+        const files = await fs.readdir(PathToCqlFiles);
+
+        for (const file of files) {
+            await fs.unlink(path.join(PathToCqlFiles, file));
+        }
     }
 
     for (const schema of Object.keys(schemas)) {
-        const { CqlTable, indexes, Types } = schemas[schema];
+        const { CqlTable, Indexes, Types } = schemas[schema];
+
+        console.time(`Time to generate ${schema}.cql`);
 
         if (dryRun) {
             console.log(CqlTable);
-            console.log(indexes.join('\n'));
+            console.log(Indexes.join('\n'));
             console.log(Types.join('\n'));
 
             continue;
         }
 
-        fs.writeFileSync(path.join(PathToCqlFiles, `${schema}.cql`), '');
-        fs.appendFileSync(path.join(PathToCqlFiles, `${schema}.cql`), `${Types.join('\n')}`);
-        fs.appendFileSync(path.join(PathToCqlFiles, `${schema}.cql`), `\n${CqlTable}`);
+        await fs.writeFile(path.join(PathToCqlFiles, `${schema}.cql`), '');
+        await fs.appendFile(path.join(PathToCqlFiles, `${schema}.cql`), `${Types.length > 0 ? Types.join('\n') : ''}`);
+        await fs.appendFile(path.join(PathToCqlFiles, `${schema}.cql`), `${Types.length > 0 ? '\n' : ''}${CqlTable}\n`);
+        await fs.appendFile(path.join(PathToCqlFiles, `${schema}.cql`), `${Indexes.length > 0 ? '\n' : ''}${Indexes.join('\n')}\n`);
 
-        if (indexes.length > 0) {
-            fs.writeFileSync(path.join(PathToCqlFiles, `${schema}_indexes.cql`), indexes.join('\n'));
+        if (clearExtraNewLines) {
+            const file = await fs.readFile(path.join(PathToCqlFiles, `${schema}.cql`), 'utf8');
+
+            const lines = file.split('\n');
+            const newLines = [];
+
+            for (let i = 0; i < lines.length; i++) {
+                const line = lines[i];
+
+                if (line === '') {
+                    if (lines[i - 1] === '' || lines[i + 1] === '') {
+                        lines.splice(i, 1);
+                    }
+                }
+
+                newLines.push(line);
+            }
+
+            await fs.writeFile(path.join(PathToCqlFiles, `${schema}.cql`), newLines.join('\n'));
         }
 
-        if (removeExtraNewLines) {
-            const file = fs.readFileSync(path.join(PathToCqlFiles, `${schema}.cql`), 'utf8');
-
-            fs.writeFileSync(path.join(PathToCqlFiles, `${schema}.cql`), file.replace(/\n\n\n/g, ''));
-        }
+        console.timeEnd(`Time to generate ${schema}.cql`);
     }
+
+    console.log('='.repeat(80));
+
+    console.timeEnd('Time to generate CQL files');
 };
 
 
